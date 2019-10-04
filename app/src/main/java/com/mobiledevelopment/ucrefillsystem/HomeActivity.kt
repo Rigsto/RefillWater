100d27b696cf6f43b653c0b6006f1e3d13ecccb4
Rigsto <rigsto@users.noreply.github.com>
2019-10-04 22:24:31 +0700

    home view completed

--- app/src/main/java/com/mobiledevelopment/ucrefillsystem/HomeActivity.kt
+++ app/src/main/java/com/mobiledevelopment/ucrefillsystem/HomeActivity.kt
@@ -1,12 +1,44 @@
 package com.mobiledevelopment.ucrefillsystem
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mobiledevelopment.ucrefillsystem.fragment.AccountFragment
+import com.mobiledevelopment.ucrefillsystem.fragment.HistoryFragment
+import com.mobiledevelopment.ucrefillsystem.fragment.HomeFragment
+import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
-
+class HomeActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+
+        loadFragment(HomeFragment())
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.bnv_item_home -> {
+                loadFragment(HomeFragment())
+                return true
+            }
+            R.id.bnv_item_history -> {
+                loadFragment(HistoryFragment())
+                return true
+            }
+            R.id.bnv_item_account -> {
+                loadFragment(AccountFragment())
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun loadFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fl_home_contaier, fragment).commit()
     }
 }
---
+++ app/src/main/java/com/mobiledevelopment/ucrefillsystem/fragment/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.mobiledevelopment.ucrefillsystem.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mobiledevelopment.ucrefillsystem.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+}
