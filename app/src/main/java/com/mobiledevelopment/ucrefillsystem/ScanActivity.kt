7ed84f07911975d507512e2c59174c0043206a9e
Rigsto <rigsto@users.noreply.github.com>
2019-10-08 15:46:14 +0700

    change scan system from fragment to activity

---
+++ app/src/main/java/com/mobiledevelopment/ucrefillsystem/ScanActivity.kt
@@ -0,0 +1,67 @@
+package com.mobiledevelopment.ucrefillsystem
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import kotlinx.android.synthetic.main.activity_scan.*
+
+class ScanActivity : AppCompatActivity() {
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+
+        codeScanner = CodeScanner(this, sqr_refill)
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                val data = it.text
+                if (checkScan(data)) {
+                    data.toInt()
+                } else {
+
+                }
+            }
+        }
+
+        codeScanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                Toast.makeText(
+                    this,
+                    "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        codeScanner.startPreview()
+
+        supportActionBar?.hide()
+        tool_bar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp)
+        tool_bar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun checkScan(data: String): Boolean {
+        try {
+            val temp = data.toInt()
+            return temp in 100..1299
+        } catch (e: NumberFormatException) {
+            return false
+        }
+    }
+}
